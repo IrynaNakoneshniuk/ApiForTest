c9efc785f412fa7cfa712daff80a95e2f4e39b12
Ірина Наконешнюк <nakonirina1996@gmail.com>
2024-10-24 15:08:45 +0300

    Add project files.

---
+++ Controllers/MyEntityController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ApiForTest.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MyEntityController : ControllerBase
+{
+    private readonly TestContext _context;
+
+    public MyEntityController(TestContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("seed")]
+    public async Task<IActionResult> SeedData()
+    {
+        var myEntity = new Author
+        {
+            Name = "Test",
+            Contact = new List<ContactInfo>
+                {
+                    new ContactInfo { Phone = "123-456-7890", Email = "example1@example.com" },
+                    new ContactInfo { Phone = "098-765-4321", Email = "example2@example.com" }
+                }
+        };
+
+        await _context.Author.AddAsync(myEntity);
+        await _context.SaveChangesAsync();
+
+        return Ok("Test data seeded.");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Author>>> GetAll()
+    {
+        var myEntities = await _context.Author.Where(a => a.Contact.Contains(new ContactInfo() { Email = "example1@example.com", Phone = "123-456-7890" })).Select(p => p.Contact).ToListAsync();
+        return Ok(myEntities);
+    }
+    
+}
+
---
+++ MyDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+using System.Text.Json;
+using System.Xml;
+
+namespace ApiForTest;
+
+public partial class TestContext : DbContext
+{
+
+    public DbSet<Author> Author { get; set; }
+
+    public TestContext(DbContextOptions<TestContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "server=localhost;port=3306;uid=root;pwd=1111;database=test;";
+        optionsBuilder
+            .UseMySql(
+                 connectionString,
+                   new MySqlServerVersion(new Version(8, 0, 27)),
+                   option => option.UseMicrosoftJson()) 
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Author>().
+            Property(p => p.Contact)
+            .HasColumnType("json");
+
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+
+public class ContactInfo
+{
+    public string Phone { get; set; }
+    public string Email { get; set; }
+}
+
+public class Author
+{
+   
+    public int Id { get; private set; }
+    public string Name { get; set; }
+    public List<ContactInfo> Contact { get; set; }
+}
